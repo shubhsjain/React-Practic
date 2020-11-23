2d893bf8082377406bbc92c714d0681f43366ca1
shubhsjain <svj8866@gmail.com>
2020-11-23 17:54:29 +0530

    use of index as key, should be reconsiderate or it can harm your UI

--- src/components/NameList.js
+++ src/components/NameList.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import Person from './Person'
+// import Person from './Person'
 
 // function NameList() {
 //     const names =['Bruce', 'Clark', 'Diana']
@@ -12,34 +12,64 @@ import Person from './Person'
 //     )
 // }
 
+// function NameList() {
+//     const person =[
+//         {
+//         id : 1,
+//         name :'Bruce',
+//         age : 20,
+//         skill : 'React'
+//     },
+//     {
+//         id : 2,
+//         name :'Clark',
+//         age : 24,
+//         skill : 'Angular'
+//     },
+//     {
+//         id : 3,
+//         name :'Diana',
+//         age : 28,
+//         skill : 'Vue'
+//     },
+//     ]
+// const personList = person.map(person =>( 
+// <Person key = {person.id} person={person}></Person> // the key prop shoud be unique in the property like id='1' in this the no. 1 shud not repeat itself in that property
+//     ))
+//     return (
+//         <div>
+//             {personList}
+//         </div>
+//     )
+// }
+
+// export default NameList
+
 function NameList() {
-    const person =[
-        {
-        id : 1,
-        name :'Bruce',
-        age : 20,
-        skill : 'React'
-    },
-    {
-        id : 2,
-        name :'Clark',
-        age : 24,
-        skill : 'Angular'
-    },
-    {
-        id : 3,
-        name :'Diana',
-        age : 28,
-        skill : 'Vue'
-    },
-    ]
-const personList = person.map(person =>( 
-<Person key = {person.id} person={person}></Person> // the key prop shoud be unique in the property like id='1' in this the no. 1 shud not repeat itself in that property
-    ))
+    const names = ['Bruce', 'Clark', 'Diana', 'Bruce']
+    // const person =[
+    //     {
+    //     id : 1,
+    //     name :'Bruce',
+    //     age : 20,
+    //     skill : 'React'
+    // },
+    // {
+    //     id : 2,
+    //     name :'Clark',
+    //     age : 24,
+    //     skill : 'Angular'
+    // },
+    // {
+    //     id : 3,
+    //     name :'Diana',
+    //     age : 28,
+    //     skill : 'Vue'
+    // },
+    // ]
+const nameList = names.map((name, index) => <h2 key = {index}> {index} {name} </h2>) // but using index as a key will cost an ugly UI. certain conditions while using key.      (1) when the items in the list do not have unique id.     (2) the list is static and will not change.     (3) the list will never be reordered os filtered ONLY WHEN THESE CONDITIONS ARE SATISFIED THEN ONLY YOU CAN USE INDEX AS KEY
     return (
-        <div>
-            {personList}
-        </div>
+        <div>{nameList}</div>
     )
 }
 
