b3ecb92012832d7e332124ad5cf34397c4a1fc4c
shubhsjain <svj8866@gmail.com>
2020-11-23 19:04:21 +0530

    styling and css basics

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 //import logo from './logo.svg';
 //import './App.css';
-import React from 'react';
-import NameList from './components/NameList';
-
 /**{import {ClassComp, ClassComp1} from './components/ClassComp';
 import FC from './components/FunctionalComp';
 import Click from './components/Click'
@@ -15,11 +12,15 @@ import Welcome from './components/Welcome'
 import Message from './components/Message'
 import ParentComp from './components/ParentComp'
 import UserGreeting from './components/UserGreeting';
+import NameList from './components/NameList';
 */
+import React from 'react';
+import Stylesheet from './components/Stylesheet';
+
 function App() {
   return (
-   <div>
-
+   <div className="App">
+     <Stylesheet />
    {/**    
    <FC/>
    <ClassComp/>
@@ -31,6 +32,7 @@ function App() {
    <Message/>
    <ParentComp/>
    <UserGreeting/>
+   <NameList/>
 
    
    <Greet name= "Clark" heroName="Superman"><button>Allow</button></Greet>
@@ -44,7 +46,6 @@ function App() {
 
    <Welcome name= "Bruce" heroName="Batman"><p>This is Children props </p></Welcome>
  */}    
-  <NameList/>
  </div>
   );
 }
---
+++ src/components/Stylesheet.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import './myStyles.css'
+import styles from './appStyles.module.css' 
+
+const heading = {
+    fontsize: '72px',
+    color: 'blue'
+}
+
+function Stylesheet(props) {
+    // let className = props.primary ? 'primary' : ''
+    return (
+        <div>
+            {/* <h1 className={`${className} font-xl`}>Stylesheets</h1> */}
+            
+
+            {/* <h2 style={{color: "blue"}}>shubh</h2> */}
+
+                {/* inline css  */}
+
+            {/* <h1 style={heading}>inline</h1> */}
+
+            {/* module.css */}
+
+            <h1 className = {styles.success}>SHUBHYA</h1>
+
+        </div>
+    )
+}
+
+export default Stylesheet
