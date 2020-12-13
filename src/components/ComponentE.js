0a3d1c6cf199240524da73f4cc1f469410e43f1c
shubhsjain <svj8866@gmail.com>
2020-12-13 20:30:04 +0530

    context

--- src/components/ComponentE.js
+++ src/components/ComponentE.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react'
 import ComponentF from './ComponentF'
+import UserContext from './userContext'
 
 export class ComponentE extends Component {
+
+    static contextType = UserContext
+    // IN OPTION TO TO ABOVE CODE WE CAN USE ComponentE.contextType = UserContext      the above code works only with class component
+
     render() {
         return (
             <div>
+                Component E context {this.context}
                 <ComponentF/>
             </div>
         )
--- src/components/userContext.js
+++ src/components/userContext.js
@@ -1,8 +1,10 @@
 import React from 'react'
 
-const UserContext = React.createContext()
+const UserContext = React.createContext('shubh') 
+// THIS IS (SHUBH) IS DEFAULT WHEN YOU DID NOT PUT THE VALUE IN APP.JS AND DONT NEED TO STATE THE COMPONENT
 
 const UserProvider = UserContext.Provider
 const UserConsumer = UserContext.Consumer
 
 export { UserProvider, UserConsumer }
+export default UserContext
